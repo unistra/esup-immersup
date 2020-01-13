2ed944ac75f0dbe8a45abe2283177c6726347c48
Thomas Baguet <baguet@unistra.fr>
2020-01-13 17:00:02 +0100

    Add tiny unit tests for building model

--- immersionlyceens/apps/core/tests/test_models.py
+++ immersionlyceens/apps/core/tests/test_models.py
@@ -3,7 +3,7 @@ from django.core.exceptions import ValidationError
 from django.test import TestCase
 from django.utils.translation import ugettext_lazy as _
 
-from ..models import Campus
+from ..models import Building, Campus
 
 
 class CampusTestCase(TestCase):
@@ -15,3 +15,8 @@ class CampusTestCase(TestCase):
     def test_campus_creation_activated(self):
         test_campus = Campus.objects.create(label='MyCampus')
         self.assertEqual(test_campus.active, True)
+
+    def test_building_str(self):
+        test_campus = Campus.objects.create(label='MyCampus')
+        test_building = Building.objects.create(label='MyBuilding', campus=test_campus)
+        self.assertEqual(str(test_building), 'MyBuilding')
