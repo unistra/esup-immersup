298a7bb4301fbd76d5f5e98b80600ad7b6747685
Christophe Boccheciampe <cb at unistra.fr>
2020-01-15 10:43:55 +0100

    Updated requirements and fabfile

--- fabfile.py
+++ fabfile.py
@@ -22,7 +22,7 @@ env.remote_virtualenv_root = join(env.remote_home, '.virtualenvs')  # venv root
 env.remote_virtualenv_dir = join(env.remote_virtualenv_root,
                                  env.application_name)  # venv for webapp dir
 # git repository url
-env.remote_repo_url = 'git@git.net:immersionlyceens.git'
+env.remote_repo_url = 'git@git.unistra.fr:di/immersionlyceens.git'
 env.local_tmp_dir = '/tmp'  # tmp dir
 env.remote_static_root = '/var/www/static/'  # root of static files
 env.locale = 'fr_FR.UTF-8'  # locale to use on remote
@@ -37,7 +37,7 @@ env.verbose_output = False  # True for verbose output
 # env.dest_path = '' # if not set using env_local_tmp_dir
 # env.excluded_files = ['pron.jpg'] # file(s) that rsync should exclude when deploying app
 # env.extra_ppa_to_install = ['ppa:vincent-c/ponysay'] # extra ppa source(s) to use
-# env.extra_pkg_to_install = ['ponysay'] # extra debian/ubuntu package(s) to install on remote
+env.extra_pkg_to_install = ['python3.6-dev'] # extra debian/ubuntu package(s) to install on remote
 # env.cfg_shared_files = ['config','/app/path/to/config/config_file'] # config files to be placed in shared config dir
 # env.extra_symlink_dirs = ['mydir','/app/mydir'] # dirs to be symlinked in shared directory
 # env.verbose = True # verbose display for pydiploy default value = True
