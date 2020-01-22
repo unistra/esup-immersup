d35578a19e8da7139079a947f7a7236715e857df
a.combeau <a.combeau@unistra.fr>
2020-01-22 15:56:14 +0100

    Added import view

--- immersionlyceens/apps/core/views.py
+++ immersionlyceens/apps/core/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
 
 # Create your views here.
+
+# TODO: !!!!!!!!!!!!!!!!!!!!!!! AUTHORIZATION REQUIRED !!!!!!!!!!!!!!!!!!!!!!!
+def import_holidays(request):
+    """Import holidays from API if it's convigured"""
+
+    # TODO: dynamic redirect
+    return redirect('/admin/core/holiday')
