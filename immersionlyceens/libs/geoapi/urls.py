d854a246814a5b4b5cc3a75b4a758076509c547f
Thomas Baguet <baguet@unistra.fr>
2020-01-24 13:31:41 +0100

    Fix bug in geoapi

--- immersionlyceens/libs/geoapi/urls.py
+++ immersionlyceens/libs/geoapi/urls.py
@@ -8,6 +8,6 @@ from . import views
 
 urlpatterns = [
     path('departments', views.ajax_get_departments, name='ajax_get_departments'),
-    path('cities/<int:dep>', views.ajax_get_cities, name='ajax_get_cities'),
-    path('zipcodes/<int:dep>/<str:city>',views.ajax_get_zipcodes, name='ajax_get_zipcodes'),
+    path('cities/<str:dep>', views.ajax_get_cities, name='ajax_get_cities'),
+    path('zipcodes/<str:dep>/<str:city>', views.ajax_get_zipcodes, name='ajax_get_zipcodes'),
 ]
