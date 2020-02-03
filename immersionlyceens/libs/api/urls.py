65c61524983b12ec0fe9fb83ac43c5aa8b67edff
Christophe Boccheciampe <cb at unistra.fr>
2020-02-03 09:26:37 +0100

    Fix url and components default manager

--- immersionlyceens/libs/api/urls.py
+++ immersionlyceens/libs/api/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
     path('get_person', views.ajax_get_person, name='PersonByName'),
     path('get_available_vars/<int:template_id>', views.ajax_get_available_vars, name='GetAvailableVars'),
     path('get_courses/<int:component_id>/', views.ajax_get_courses, name='GetCourses'),
-    path('get_available_documents/', views.get_ajax_documents, name='get_available_documents',)
+    path('get_available_documents/', views.get_ajax_documents, name='get_available_documents'),
     path('get_slots/<int:component>', views.get_ajax_slots, name='get_slots',)
 ]
