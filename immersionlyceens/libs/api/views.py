2d55b38976b06719ea9062562feb1f615a43bf48
a.combeau <a.combeau@unistra.fr>
2020-02-10 14:55:32 +0100

    Added handle for blank flields in slot datatable list

--- immersionlyceens/libs/api/views.py
+++ immersionlyceens/libs/api/views.py
@@ -147,28 +147,33 @@ def get_ajax_slots(request, component=None):
     response = {'msg': '', 'data': []}
     if component:
         slots = Slot.objects.filter(course__training__components__id=component)
+        all_data = []
+        for slot in slots:
+            data = {
+                'id': slot.id,
+                'published': slot.published,
+                'course_label': slot.course.label,
+                'course_type': slot.course_type.label if slot.course_type is not None else '-',
+                'date': slot.date.strftime('%a %d-%m-%Y') if slot.date is not None else '-',
+                'time': '{s} - {e}'.format(
+                    s=slot.start_time.strftime('%Hh%M') or '',
+                    e=slot.end_time.strftime('%Hh%M') or '',
+                ) if slot.start_time is not None and slot.end_time is not None else '-',
+                'building': '{} - {}'.format(
+                    slot.building.label,
+                    slot.campus.label
+                ) if slot.building is not None and slot.campus is not None else '-',
+                'room': slot.room if slot.room is not None else '-',
+                'teachers': ', '.join([
+                    '{} {}'.format(e.first_name, e.last_name.upper())
+                    for e in slot.teachers.all()]),
+                'n_register': 10, # todo: registration count
+                'n_places': slot.n_places if slot.n_places is not None and slot.n_places > 0 else '-',
+                'additional_information': slot.additional_information,
+            }
+            all_data.append(data)
 
-        data = [{
-            'id': slot.id,
-            'published': slot.published,
-            'course_label': slot.course.label,
-            'course_type': slot.course_type.label,
-            'date': slot.date.strftime('%a %d-%d-%Y'),
-            'time': '{s} - {e}'.format(
-                s=slot.start_time.strftime('%Hh%M'),
-                e=slot.end_time.strftime('%Hh%M'),
-            ),
-            'building': '' + slot.building.label + ' - ' + slot.campus.label,
-            'room': slot.room,
-            'teachers': ', '.join([
-                '{} {}'.format(e.first_name, e.last_name.upper())
-                for e in slot.teachers.all()]),
-            'n_register': 10,
-            'n_places': slot.n_places,
-            'additional_information': slot.additional_information,
-        } for slot in slots]
-
-        response['data'] = data
+        response['data'] = all_data
     else:
         response['msg'] = gettext('Error : component id')
 
