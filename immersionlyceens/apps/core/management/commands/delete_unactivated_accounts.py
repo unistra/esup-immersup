2dae7b96b3ccfa2d9a2b540efec36722f1fd0f4a
Christophe Boccheciampe <cb at unistra.fr>
2020-02-19 14:16:02 +0100

    Command added to delete unactivated accounts after 'destruction_date'

---
+++ immersionlyceens/apps/core/management/commands/delete_unactivated_accounts.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+"""
+Delete unactivated accounts
+"""
+import logging
+
+from datetime import datetime
+from django.core.management.base import BaseCommand, CommandError
+from django.utils.translation import ugettext_lazy as _
+from ...models import ImmersionUser
+
+logger = logging.getLogger(__name__)
+
+class Command(BaseCommand):
+    """ Import components from Abyla
+    """
+
+    def handle(self, *args, **options):
+        today = datetime.today().date()
+
+        deleted = ImmersionUser.objects.filter(
+            destruction_date__isnull=False,
+            destruction_date__lt=today).delete()
+
+        if deleted[0]:
+            logger.info(_("{} account(s) deleted".format(deleted[0])))
+        else:
+            logger.info(_("No account to delete"))
