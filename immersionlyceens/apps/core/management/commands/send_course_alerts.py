408d212df4dcc199041711918a61a6dda24bcbd9
Christophe Boccheciampe <cb at unistra.fr>
2020-03-31 14:40:37 +0200

    New command to send courses free slots notifications based on user alerts

---
+++ immersionlyceens/apps/core/management/commands/send_course_alerts.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+"""
+Send a notification when a slot is available for registration
+This command is meant to be run in the evening
+"""
+import logging
+
+import datetime
+from django.core.management.base import BaseCommand, CommandError
+from django.utils.translation import ugettext_lazy as _
+from immersionlyceens.libs.mails.utils import send_email
+
+from ...models import Course, MailTemplate, UserCourseAlert
+
+logger = logging.getLogger(__name__)
+
+class Command(BaseCommand):
+    """
+    """
+
+    def handle(self, *args, **options):
+        template_code = 'ALERTE_DISPO'
+        courses_dict = {}
+        today = datetime.datetime.today().date()
+
+        # Email template
+        try:
+            template = MailTemplate.objects.get(code=template_code, active=True)
+            logger.debug("Template found : %s" % template)
+        except MailTemplate.DoesNotExist:
+            logger.error(
+                "Cannot find an active template named '%s'. Please check the Messages Templates in admin section.",
+                template_code)
+            return
+
+        courses = Course.objects.prefetch_related('slots').filter(slots__date__gt=today)
+
+        for course in courses:
+            slot_list = []
+            for slot in course.slots.filter(date__gt=today).order_by('date', 'start_time'):
+                if slot.available_seats() > 0:
+                    slot_list.append(slot)
+
+            if slot_list:
+                courses_dict[course.id] = slot_list.copy()
+
+        alerts = UserCourseAlert.objects.filter(course__id__in=courses_dict.keys(), email_sent=False)
+
+        for alert in alerts:
+            slots = courses_dict[alert.course.id]
+            try:
+                message_body = template.parse_vars(user=None, request=None, slot_list=slots, course=alert.course)
+                logger.debug("Message body : %s" % message_body)
+                send_email(alert.email, template.subject, message_body)
+                alert.email_sent = True
+                alert.save()
+            except Exception as e:
+                logger.exception(e)
+                logger.error("Cannot send email to %s : '%s'", alert.email, e)
+
+            return
