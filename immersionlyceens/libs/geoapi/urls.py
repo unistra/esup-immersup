8d447fe65bcaa5c9773e9ac662b289eb6f5e5435
Thomas Baguet <baguet@unistra.fr>
2020-04-08 17:38:46 +0200

    Sort urls in geoapi

--- immersionlyceens/libs/geoapi/urls.py
+++ immersionlyceens/libs/geoapi/urls.py
@@ -1,5 +1,5 @@
 """
-Url configuration for the core application API
+Url configuration for the geoapi API
 """
 
 from django.urls import path
@@ -7,7 +7,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('departments', views.ajax_get_departments, name='ajax_get_departments'),
     path('cities/<str:dep>', views.ajax_get_cities, name='ajax_get_cities'),
+    path('departments', views.ajax_get_departments, name='ajax_get_departments'),
     path('zipcodes/<str:dep>/<str:city>', views.ajax_get_zipcodes, name='ajax_get_zipcodes'),
 ]
