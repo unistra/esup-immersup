77dcc7beab04172c683024742c0f0750a3087917
Thomas Baguet <baguet@unistra.fr>
2020-04-22 10:31:19 +0200

    Using form-control class for course form for responsive stuff still pb with submit btn

--- immersionlyceens/apps/core/forms.py
+++ immersionlyceens/apps/core/forms.py
@@ -1,15 +1,18 @@
 from datetime import datetime
+
 from django import forms
 from django.conf import settings
+from django.forms.widgets import DateInput, TimeInput
 from django.utils.translation import ugettext_lazy as _
 from django.utils.translation import ungettext
-from django.forms.widgets import DateInput, TimeInput
-from django_summernote.widgets import SummernoteWidget, SummernoteInplaceWidget
+from django_summernote.widgets import SummernoteInplaceWidget, SummernoteWidget
 
-from .admin_forms import HighSchoolForm
-from .models import (Course, Component, Training, ImmersionUser, UniversityYear, Slot, Calendar,
-                     CourseType, Campus, Building, HighSchool)
 from ..immersion.forms import StudentRecordForm
+from .admin_forms import HighSchoolForm
+from .models import (
+    Building, Calendar, Campus, Component, Course, CourseType,
+    HighSchool, ImmersionUser, Slot, Training, UniversityYear,
+)
 
 
 class CourseForm(forms.ModelForm):
@@ -17,6 +20,9 @@ class CourseForm(forms.ModelForm):
         self.request = kwargs.pop("request")
         super().__init__(*args, **kwargs)
         self.fields["training"].queryset = self.fields["training"].queryset.filter(active=True)
+
+        for field_name, field in self.fields.items():
+            field.widget.attrs.update({'class': 'form-control'})
 
         if self.request:
             allowed_comps = Component.activated.user_cmps(self.request.user, 'SCUIO-IP')
@@ -37,19 +43,17 @@ class CourseForm(forms.ModelForm):
         try:
             active_year = UniversityYear.objects.get(active=True)
         except UniversityYear.MultipleObjectsReturned:
-            raise forms.ValidationError(
-                _("Error : multiple active university years"))
+            raise forms.ValidationError(_("Error : multiple active university years"))
         except UniversityYear.DoesNotExist:
-            raise forms.ValidationError(
-                _("Error : can't find any active university year"))
+            raise forms.ValidationError(_("Error : can't find any active university year"))
 
         if active_year.start_date and active_year.end_date:
             if not (active_year.start_date <= datetime.today().date() <= active_year.end_date):
                 raise forms.ValidationError(
-                    _("Error : a course can only be created between start and end of the active university year"))
+                    _("Error : a course can only be created between start and end of the active university year")
+                )
         else:
-            raise forms.ValidationError(
-                _("Error : dates of active university year improperly configured"))
+            raise forms.ValidationError(_("Error : dates of active university year improperly configured"))
 
         # Check user rights
         if self.request:
@@ -58,8 +62,7 @@ class CourseForm(forms.ModelForm):
             course_comps = training.components.all()
 
             if not (course_comps & allowed_comps).exists():
-                raise forms.ValidationError(
-                    _("You don't have enough privileges to update this course"))
+                raise forms.ValidationError(_("You don't have enough privileges to update this course"))
 
         return cleaned_data
 
@@ -69,16 +72,24 @@ class CourseForm(forms.ModelForm):
 
 
 class SlotForm(forms.ModelForm):
-
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         instance = kwargs.get('instance', None)
 
         # self.fields["training"].queryset = self.fields["training"].queryset.filter(active=True)
 
-        for elem in ['course', 'course_type', 'campus', 'building',
-                  'room', 'start_time', 'end_time', 'n_places',
-                  'additional_information', 'published',]:
+        for elem in [
+            'course',
+            'course_type',
+            'campus',
+            'building',
+            'room',
+            'start_time',
+            'end_time',
+            'n_places',
+            'additional_information',
+            'published',
+        ]:
             self.fields[elem].widget.attrs.update({'class': 'form-control'})
 
         # course type filter
@@ -87,7 +98,6 @@ class SlotForm(forms.ModelForm):
         self.fields['campus'].queryset = Campus.objects.filter(active=True).order_by('label')
         # building filter
         self.fields['building'].queryset = Building.objects.filter(active=True).order_by('label')
-
 
         if instance:
             self.fields['date'].value = instance.date
@@ -102,30 +112,28 @@ class SlotForm(forms.ModelForm):
         if cals.count() > 0:
             cal = cals[0]
         if not cal:
-            raise forms.ValidationError(
-                _('Error: A calendar is required to set a slot.')
-            )
+            raise forms.ValidationError(_('Error: A calendar is required to set a slot.'))
 
         pub = cleaned_data.get('published')
         if pub is not None:
             if pub:
                 # Mandatory fields
-                if not all(cleaned_data.get(e) for e in ['course', 'course_type', 'campus',
-                            'building', 'room', 'date', 'start_time', 'end_time',]):
+                if not all(
+                    cleaned_data.get(e)
+                    for e in ['course', 'course_type', 'campus', 'building', 'room', 'date', 'start_time', 'end_time',]
+                ):
                     raise forms.ValidationError(_('Required fields are not filled in'))
 
         _date = cleaned_data.get('date')
         if _date and not cal.date_is_between(_date):
-            raise forms.ValidationError({
-                'date': _('Error: The date must be between the dates of the current calendar')
-                })
+            raise forms.ValidationError(
+                {'date': _('Error: The date must be between the dates of the current calendar')}
+            )
 
         start_time = cleaned_data.get('start_time')
         end_time = cleaned_data.get('end_time')
         if start_time and end_time and start_time >= end_time:
-            raise forms.ValidationError({
-                'start_time': _('Error: Start time must be set before end time')
-            })
+            raise forms.ValidationError({'start_time': _('Error: Start time must be set before end time')})
 
         if course and not course.published and pub:
             course.published = True
@@ -135,19 +143,27 @@ class SlotForm(forms.ModelForm):
 
     class Meta:
         model = Slot
-        fields = ('id', 'course', 'course_type', 'campus', 'building',
-                  'room', 'date', 'start_time', 'end_time', 'n_places',
-                  'additional_information', 'published',)
+        fields = (
+            'id',
+            'course',
+            'course_type',
+            'campus',
+            'building',
+            'room',
+            'date',
+            'start_time',
+            'end_time',
+            'n_places',
+            'additional_information',
+            'published',
+        )
         widgets = {
-            'additional_information': forms.Textarea(attrs={
-                'placeholder': _('Input additional information'),
-            }),
+            'additional_information': forms.Textarea(attrs={'placeholder': _('Input additional information'),}),
             'n_places': forms.NumberInput(attrs={'min': 1, 'max': 200, 'value': 0}),
             'room': forms.TextInput(attrs={'placeholder': _('Input the room name')}),
-            'date': forms.DateInput(format='%d/%m/%Y', attrs={
-                'placeholder': _('dd/mm/yyyy'),
-                'class': 'datepicker form-control'
-            }),
+            'date': forms.DateInput(
+                format='%d/%m/%Y', attrs={'placeholder': _('dd/mm/yyyy'), 'class': 'datepicker form-control'}
+            ),
             'start_time': TimeInput(format='%H:%M'),
             'end_time': TimeInput(format='%H:%M'),
         }
@@ -159,19 +175,34 @@ class MyHighSchoolForm(HighSchoolForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        for elem in ['label', 'address', 'address2', 'address3',
-                     'department', 'city', 'zip_code',
-                     'fax', 'email', 'phone_number',
-                     'head_teacher_name',
-                     ]:
+        for elem in [
+            'label',
+            'address',
+            'address2',
+            'address3',
+            'department',
+            'city',
+            'zip_code',
+            'fax',
+            'email',
+            'phone_number',
+            'head_teacher_name',
+        ]:
             self.fields[elem].widget.attrs.update({'class': 'form-control'})
 
     class Meta:
         model = HighSchool
         fields = [
-            'label', 'address', 'address2', 'address3',
-            'department', 'city', 'zip_code',
-            'fax', 'email', 'phone_number',
+            'label',
+            'address',
+            'address2',
+            'address3',
+            'department',
+            'city',
+            'zip_code',
+            'fax',
+            'email',
+            'phone_number',
             'head_teacher_name',
         ]
 
@@ -205,8 +236,10 @@ class ContactForm(forms.Form):
 
         self.fields['subject'] = forms.CharField(label=_("Subject"), max_length=100, required=True)
 
-        self.fields['body'] = forms.CharField(widget=SummernoteInplaceWidget(
-            attrs={'summernote': {'width': '100%', 'height': '200px', 'rows': 6, 'airMode': False, }})
+        self.fields['body'] = forms.CharField(
+            widget=SummernoteInplaceWidget(
+                attrs={'summernote': {'width': '100%', 'height': '200px', 'rows': 6, 'airMode': False,}}
+            )
         )
 
         #
@@ -221,4 +254,6 @@ class ComponentForm(forms.ModelForm):
 
     class Meta:
         model = Component
-        fields = ['mailing_list', ]
+        fields = [
+            'mailing_list',
+        ]
