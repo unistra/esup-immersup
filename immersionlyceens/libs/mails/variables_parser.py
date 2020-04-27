35258724e1937f5d69f21531057871ad797034f6
Thomas Baguet <baguet@unistra.fr>
2020-04-27 17:26:24 +0200

    Remove bad condition for certificate dl in variables_parser

--- immersionlyceens/libs/mails/variables_parser.py
+++ immersionlyceens/libs/mails/variables_parser.py
@@ -155,9 +155,6 @@ def parser(message_body, available_vars=None, user=None, request=None, **kwargs)
                 )
             except HighSchoolStudentRecord.DoesNotExist:
                 pass
-        elif user.highschool:
-            vars.append(('${lycee}', user.highschool.label))
-            vars.append(('${etudiant_date_naissance}', date_format(user.highschool.birth_date, 'd/m/Y')))
         elif user.is_student():
             # TODO: maybe instead of lycee use a home_institution tpl var ???
             vars.append(('${lycee}', institution_label))
