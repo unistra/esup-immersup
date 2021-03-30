0c6b158118c9f704de7ce1977ea6b83154852967
Thomas Baguet <baguet@unistra.fr>
2021-03-30 09:18:26 +0200

    Enhance version nb system and add get_version()

---
+++ immersionlyceens/__init__.py
@@ -0,0 +1,9 @@
+
+VERSION = (1, 0, 0)
+
+
+def get_version():
+    if not VERSION[2] and len(VERSION) > 3:
+        return f"{'.'.join(map(str, VERSION[:2]))}{VERSION[3]}"
+    else:
+        return '.'.join(map(str, VERSION[:3]))
