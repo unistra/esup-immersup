fc09f5a3f3ae2bcb43a461135bc084444eabe08b
Thomas Baguet <baguet@unistra.fr>
2021-09-30 20:47:29 +0200

    Optimise container creation : run stuff at the end

--- deploy/docker/app/entrypoint.sh
+++ deploy/docker/app/entrypoint.sh
@@ -2,13 +2,6 @@
 
 set -e
 
-
-echo "Waiting for postgres..."
-
-while !</dev/tcp/db/5432; do sleep 3; done;
-
-echo "PostgreSQL started"
-
 # python manage.py flush --no-input
 python manage.py migrate
 # python manage.py collectstatic --noinput
