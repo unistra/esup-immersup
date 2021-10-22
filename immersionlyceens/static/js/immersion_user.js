5c1990c17d1023a9e197f80db1d97d8421eb760e
Christophe Boccheciampe <cb@unistra.fr>
2021-10-22 10:12:23 +0200

    Fix JS query order

--- immersionlyceens/static/js/immersion_user.js
+++ immersionlyceens/static/js/immersion_user.js
@@ -84,7 +84,6 @@ $(document).ready(function() {
           success : function(json) {
             let msg = json['msg'] !== undefined ? json['msg'] : ""
             let query = json['data'] !== undefined ? json['data'][0] : ""
-            results = json['data'] !== undefined ? json['data'].slice(1) : null
 
             if(msg !== '') {
               document.getElementById('ws_message').innerHTML = msg
@@ -97,6 +96,7 @@ $(document).ready(function() {
 
             // Prevent previous (longer) query from erasing latest one
             if(query >= query_order) {
+              results = json['data'] !== undefined ? json['data'].slice(1) : null
               document.getElementById('livesearch_label').style.visibility = 'visible'
               document.getElementById('live_select').style.visibility = 'visible'
 
