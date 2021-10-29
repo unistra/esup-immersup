33c547ea9ca78c16d21e7f32c6681452061c8042
a.combeau <a.combeau@unistra.fr>
2021-10-29 15:52:28 +0200

    Added list view for training and serializer

--- immersionlyceens/apps/core/serializers.py
+++ immersionlyceens/apps/core/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
-from .models import Campus, Establishment
+from .models import Campus, Establishment, Training, TrainingSubdomain, HighSchool
+
 
 class CampusSerializer(serializers.ModelSerializer):
 
@@ -14,3 +15,29 @@ class EstablishmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Establishment
         fields = "__all__"
+
+
+class HighSchoolViewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = HighSchool
+        fields = ("id", "label")
+
+
+class TrainingSubdomainSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TrainingSubdomain
+        fields = ("id", "label", "active")
+
+
+class TrainingHighSchoolSerializer(serializers.ModelSerializer):
+    training_subdomains = serializers.SerializerMethodField("get_training_subdomains")
+    highschool = HighSchoolViewSerializer()
+
+    def get_training_subdomains(self, training):
+        query = training.training_subdomains.filter(active=True)
+        serializer = HighSchoolViewSerializer(instance=query, many=True)
+        return serializer.data
+
+    class Meta:
+        model = Training
+        fields = ("id", "label", "training_subdomains", "highschool", "active")
