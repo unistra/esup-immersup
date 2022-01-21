ccfa86be6b0704105062d2f263996b833045aa25
Christophe Boccheciampe <cb@unistra.fr>
2022-01-21 17:28:00 +0100

    Hide attendances links for not face-to-face slots

--- immersionlyceens/static/js/core/events_slots.js
+++ immersionlyceens/static/js/core/events_slots.js
@@ -208,7 +208,7 @@ function init_datatable() {
             }
 
             if(row.attendances_value === 1) {
-              element += "<button class=\"btn btn-light btn-sm mr-1\" name=\"edit\" onclick=\"open_modal("+ data +","+row.attendances_value+","+row.n_places+","+row.is_past+","+row.can_update_registrations+")\" title=\"" + attendances_text + "\">" +
+              element += "<button class=\"btn btn-light btn-sm mr-1\" name=\"edit\" onclick=\"open_modal("+ data +","+row.attendances_value+","+row.n_places+","+row.is_past+","+row.can_update_registrations+","+row.face_to_face+")\" title=\"" + attendances_text + "\">" +
                          "<i class='fa fas fa-edit fa-2x centered-icon'></i>" +
                          "</button>";
             }
