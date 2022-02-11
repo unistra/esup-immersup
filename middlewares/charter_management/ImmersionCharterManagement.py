aeca0d66fd0645e74ecd40e7bd3e14a40c07a967
Christophe Boccheciampe <cb@unistra.fr>
2022-02-11 11:28:40 +0100

    Fix student test in charter middleware

--- middlewares/charter_management/ImmersionCharterManagement.py
+++ middlewares/charter_management/ImmersionCharterManagement.py
@@ -31,7 +31,7 @@ class ImmersionCharterManagement:
             already_signed_charter = any([
                 user.is_superuser,
                 user.is_operator(),
-                user.is_student, # FIXME WHEN WE KNOW HOW TO LINK A STUDENT WITH THE ESTABLISHMENT
+                user.is_student(), # FIXME WHEN WE KNOW HOW TO LINK A STUDENT WITH THE ESTABLISHMENT
                 user.establishment and (user.establishment.master or user.establishment.signed_charter),
                 highschool and highschool.postbac_immersion and highschool.signed_charter
             ])
