798f1888b24d9723a19e53ffb6151a584f261317
Thomas Baguet <baguet@unistra.fr>
2022-02-18 16:07:51 +0100

    Cleaner way to fix attendance update for speakers

--- immersionlyceens/static/js/core/my_courses_slots.js
+++ immersionlyceens/static/js/core/my_courses_slots.js
@@ -116,7 +116,7 @@ function init_datatable() {
 
         if(row.attendances_value === 1 && (row.can_update_course_slot || row.can_update_attendances)) {
           edit_mode = 1
-          msg = "<button class=\"btn btn-light btn-sm mr-4\" name=\"edit\" onclick=\"open_modal("+ row.id +","+ edit_mode +","+row.n_places+")\" title=\"" + attendances_text + "\">" +
+          msg = "<button class=\"btn btn-light btn-sm mr-4\" name=\"edit\" onclick=\"open_modal("+ row.id +","+ edit_mode +","+row.n_places+","+row.is_past+","+row.can_update_registrations+","+row.face_to_face+")\" title=\"" + attendances_text + "\">" +
               "<i class='fa fas fa-edit fa-2x'></i>" +
               "</button>";
         }
