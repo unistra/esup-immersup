2d8d143204248f4921cb03566025fbc39618958c
Thomas Baguet <baguet@unistra.fr>
2022-03-01 08:28:57 +0100

    Fix rights on EstablishmentAdmin

--- immersionlyceens/apps/core/admin.py
+++ immersionlyceens/apps/core/admin.py
@@ -773,7 +773,7 @@ class EstablishmentAdmin(AdminWithRequest, admin.ModelAdmin):
                 'data_source_settings', 'logo', 'signature', 'objects', 'activated',
                 'address', 'address2', 'address3', 'certificate_header', 'certificate_footer'
             )
-        elif request.user.is_master_establishment_manager():
+        elif request.user.is_master_establishment_manager() and not user.is_superuser:
             return super().get_readonly_fields(request, obj) + (
                 'code', 'label', 'short_label', 'department', 'city', 'zip_code', 'phone_number', 'fax',
                 'badge_html_color', 'email', 'data_source_plugin',
