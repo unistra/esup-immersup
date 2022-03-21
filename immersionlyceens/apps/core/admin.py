191c80d1b930796357f52f2216a3d636c20b08ee
Christophe Boccheciampe <cb@unistra.fr>
2022-03-21 06:47:59 +0100

    Fix missing import in core admin

--- immersionlyceens/apps/core/admin.py
+++ immersionlyceens/apps/core/admin.py
@@ -15,6 +15,8 @@ from django_admin_listfilter_dropdown.filters import (
 from django_json_widget.widgets import JSONEditorWidget
 from django_summernote.admin import SummernoteModelAdmin
 
+from immersionlyceens.apps.immersion.models import HighSchoolStudentRecord
+
 from .admin_forms import (
     AccompanyingDocumentForm, BachelorMentionForm, BuildingForm, CalendarForm,
     CampusForm, CancelTypeForm, CertificateLogoForm, CertificateSignatureForm,
