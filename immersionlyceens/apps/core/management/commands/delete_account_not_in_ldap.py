af644b49c4c0af2e603a9942b60af31607c228a3
a.combeau <a.combeau@unistra.fr>
2022-04-26 16:51:37 +0200

    Added deletion command of accounts that are not in ldap of establishments

---
+++ immersionlyceens/apps/core/management/commands/delete_account_not_in_ldap.py
@@ -0,0 +1,22 @@
+import time
+from typing import List, Any
+
+from django.core.management import BaseCommand
+from django.db.models import QuerySet
+
+from immersionlyceens.apps.core.models import Establishment, ImmersionUser
+from immersionlyceens.libs.api.accounts import AccountAPI
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        """Delete users not present in ldap of establishments"""
+        users: QuerySet = ImmersionUser.objects.filter(
+            establishment__data_source_plugin="LDAP",
+            is_superuser=False,
+        )
+        for user in users:
+            account_api: AccountAPI = AccountAPI(user.establishment)
+            ldap_reponse: List[Any] = account_api.search_user_by_email(user.email)
+            if ldap_reponse == []:
+                user.delete()
