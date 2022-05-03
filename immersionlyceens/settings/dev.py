9b32c5c7bc657f87eabe59b67c445827e43116c6
a.combeau <a.combeau@unistra.fr>
2022-05-03 14:24:45 +0200

    Fixed environment variable setting

--- immersionlyceens/settings/dev.py
+++ immersionlyceens/settings/dev.py
@@ -104,7 +104,7 @@ EMAIL_BACKEND = 'immersionlyceens.libs.mails.backends.ConsoleBackend'
 # FORCE_EMAIL_ADDRESS = "appli-immersionlyceens-test@unistra.fr"
 
 EMAIL_HOST = environ.get('EMAIL_HOST', '127.0.0.1')
-EMAIL_USE_TLS = environ.get('EMAIL_USE_TLS', False).lower() == 'true'
+EMAIL_USE_TLS = environ.get('EMAIL_USE_TLS', "false").lower() == 'true'
 EMAIL_PORT = environ.get('EMAIL_PORT', 25)
 EMAIL_HOST_USER = environ.get('EMAIL_HOST_USER', '')
 EMAIL_HOST_PASSWORD = environ.get('EMAIL_HOST_PASSWORD', '')
