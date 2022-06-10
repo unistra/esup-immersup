ee7e661a7e0672d8a1ca4dbe627a989815733470
Christophe Boccheciampe <cb@unistra.fr>
2022-06-10 12:17:18 +0200

    master_establishment added as a context_processor to use master establishment label in main header

--- immersionlyceens/apps/context_processors.py
+++ immersionlyceens/apps/context_processors.py
@@ -1,4 +1,18 @@
+from django.utils.translation import gettext
 from immersionlyceens.apps.core.models import Establishment
 
 def establishments(request):
     return {'core_establishments': Establishment.activated.all().order_by('label')}
+
+def master_establishment(request):
+    master_establishment_label = gettext("Please add a master establishment !")
+
+    try:
+        master_establishment = Establishment.activated.get(master=True)
+        master_establishment_label = master_establishment.label
+    except Establishment.DoesNotExist:
+        pass
+
+    return {
+        'master_establishment_label': master_establishment_label
+    }
