9bb62dfc414ff0ed05abaaa42619be3e679070a0
Thomas Baguet <baguet@unistra.fr>
2023-01-16 16:21:59 +0100

    Remove deprecated s3 relative setting

--- immersionlyceens/settings/prod.py
+++ immersionlyceens/settings/prod.py
@@ -111,7 +111,6 @@ STAGE = 'Prod'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_S3_FILE_OVERWRITE = True
 AWS_DEFAULT_ACL = None
-AWS_AUTO_CREATE_BUCKET = True
 AWS_ACCESS_KEY_ID = '{{ s3_access_key }}'
 AWS_SECRET_ACCESS_KEY = '{{ s3_secret_key }}'
 AWS_STORAGE_BUCKET_NAME = '{{ s3_bucket }}'
