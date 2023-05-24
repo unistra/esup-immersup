a9e74da163ac52b8c7bb8d226fefde13fdb02f53
Christophe Boccheciampe <cb@unistra.fr>
2023-05-24 08:55:56 +0200

    New template for visitors and high school records to (re)validate

--- immersionlyceens/apps/core/management/commands/send_pending_validations_notification.py
+++ immersionlyceens/apps/core/management/commands/send_pending_validations_notification.py
@@ -7,7 +7,8 @@ import logging
 from datetime import datetime
 from django.core.management.base import BaseCommand, CommandError
 from django.utils.translation import gettext_lazy as _
-from ...models import HighSchool
+from ...models import HighSchool, ImmersionUser
+from immersionlyceens.apps.immersion.models import HighSchoolStudentRecord, VisitorRecord
 
 logger = logging.getLogger(__name__)
 
@@ -16,10 +17,41 @@ class Command(BaseCommand):
     """
 
     def handle(self, *args, **options):
-        highschools = HighSchool.objects.filter(student_records__validation=1).distinct()
+        hs_to_validate = HighSchoolStudentRecord.STATUSES["TO_VALIDATE"]
+        hs_to_revalidate = HighSchoolStudentRecord.STATUSES["TO_REVALIDATE"]
+
+        visitors_to_validate = VisitorRecord.STATUSES["TO_VALIDATE"]
+        visitors_to_revalidate = VisitorRecord.STATUSES["TO_VALIDATE"]
+
+        # High schools with conventions
+        highschools = HighSchool.objects.filter(
+            with_convention=True,
+            student_records__validation__in=[hs_to_validate, hs_to_revalidate]
+        ).distinct()
 
         for highschool in highschools:
-            referents = highschool.users.filter(groups__name='REF-LYC')
+            referents = highschool.users.prefetch_related("groups").filter(groups__name='REF-LYC')
 
             for referent in referents:
                 referent.send_message(None, 'CPT_AVALIDER_LYCEE')
+
+
+        # Visitors and high school without conventions : send reminders to establishments managers
+        hs_records = HighSchoolStudentRecord.objects\
+            .prefetch_related("highschool")\
+            .filter(
+                highschool__with_convention=False,
+                validation__in=[hs_to_validate, hs_to_revalidate]
+            )\
+            .exists()
+
+        visitor_records = VisitorRecord.objects\
+            .filter(
+                validation__in=[visitors_to_validate, visitors_to_revalidate]
+            )\
+            .exists()
+
+        if hs_records or visitor_records:
+            manager_groups = ["REF-ETAB-MAITRE", "REF-ETAB"]
+            for user in ImmersionUser.objects.prefetch_related("groups").filter(groups__name__in=manager_groups):
+                user.send_message(None, 'CPT_AVALIDER_REF_ETAB')
