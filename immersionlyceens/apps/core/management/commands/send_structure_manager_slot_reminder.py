9cfa727d5030783e6b066bdf38a4dc03ace3bd2f
Thomas Baguet <baguet@unistra.fr>
2023-07-05 15:40:07 +0200

    Ref #382 take account of true slot limit reg. date

--- immersionlyceens/apps/core/management/commands/send_structure_manager_slot_reminder.py
+++ immersionlyceens/apps/core/management/commands/send_structure_manager_slot_reminder.py
@@ -30,7 +30,7 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         success = "%s : %s" % (_("Send slot reminder"), _("success"))
         returns = []
-        today = datetime.datetime.today().date()
+        today = timezone.now()
         default_value = 4
 
         # settings / default value
@@ -49,13 +49,12 @@ class Command(BaseCommand):
         if days <= 0:
             days = default_value
 
-        slot_date = today + datetime.timedelta(days=days)
+        slot_date = today.date() + datetime.timedelta(days=days)
         slots = Slot.objects.filter(date=slot_date, published=True)
 
         for slot in slots:
             slot_structure = slot.get_structure()
-            # < !!!
-            if slot.registration_limit_date > today:
+            if slot.registration_limit_date < today:
                 str_managers = ImmersionUser.objects.filter(
                     groups__name="REF-STR",
                     structures__in=[
