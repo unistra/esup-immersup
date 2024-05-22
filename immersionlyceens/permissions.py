971535619afac2f2383e98710a556b9940757d8a
Thomas Baguet <baguet@unistra.fr>
2024-05-22 11:29:22 +0200

    Ref #439 add HighSchool hijacking for Estab. managers

--- immersionlyceens/permissions.py
+++ immersionlyceens/permissions.py
@@ -47,7 +47,7 @@ def hijack_permissions(*, hijacker, hijacked):
             # Establishment manager could hijack students, visitors and high school students from anywhere
             elif (
                 hijacker.is_establishment_manager() and
-                hijacked.is_visitor() or hijacked.is_student() or hijacked.is_high_school_student()
+                hijacked.is_high_school_manager() or hijacked.is_visitor() or hijacked.is_student() or hijacked.is_high_school_student()
             ):
                 return True
     except (ValueError, NameError):
