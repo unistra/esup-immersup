7e34204e21dc060140317b9adb4dd85a7054af56
Christophe Boccheciampe <cb@unistra.fr>
2024-07-23 15:41:18 +0200

    Restore buttons on event slots list - wait for TB to reimplement 'cohorts_only'

--- immersionlyceens/static/js/core/events_slots.js
+++ immersionlyceens/static/js/core/events_slots.js
@@ -66,6 +66,34 @@ function init_datatable() {
         render: function(data, type, row) {
           let element = ""
 
+          if (row['can_update_event_slot']) {
+            element += `<a href="/core/off_offer_event_slot/${data}/1" class="btn btn-light btn-sm mr-1" ` +
+                       `title="${duplicate_text}"><i class="fa far fa-copy fa-2x centered-icon"></i></a>`;
+
+            if(row.is_past === false) {
+              element += `<a href="/core/off_offer_event_slot/${data}" class="btn btn-light btn-sm mr-1" title="${modify_text}"><i class="fa fas fa-pencil fa-2x centered-icon"></i></a>\n`;
+            }
+            if(row.n_register === 0 && row.is_past === false) {
+              element += `<button class="btn btn-light btn-sm mr-1" onclick="deleteDialog.data('slot_id', ${data}).dialog(\'open\')" title="${delete_text}"><i class="fa fas fa-trash fa-2x centered-icon"></i></button>\n`;
+            }
+          }
+
+          if (row['can_update_attendances']) {
+            if (row.attendances_value === 1) {
+              element += `<button class="btn btn-light btn-sm mr-1" name="edit" onclick="open_modal(${data}, ${row.attendances_value}, ${row.n_places}, ${row.allow_individual_registrations}, ${row.allow_group_registrations}, ${row.group_mode}, ${row.n_group_places}, ${row.is_past}, ${row.can_update_registrations}, ${row.place})" title="${attendances_text}">` +
+                         `<i class='fa fas fa-edit fa-2x centered-icon'></i>` +
+                         `</button>`;
+            }
+            else if (row.attendances_value !== -1) {
+              element += `<button class="btn btn-light btn-sm mr-1" name="view" onclick="open_modal(${data}, ${row.attendances_value}, ${row.n_places}, ${row.allow_individual_registrations}, ${row.allow_group_registrations}, ${row.group_mode}, ${row.n_group_places}, ${row.is_past}, ${row.can_update_registrations})" title="${registered_text}">` +
+                         `<i class='fa fas fa-eye fa-2x centered-icon'></i>` +
+                         `</button>`;
+            }
+          }
+
+          /*
+          # KEEP THIS FOR FUTURE FIXES
+
           if (row.structure_code && row.structure_managed_by_me || row.highschool_label && row.highschool_managed_by_me || cohorts_only ) {
             if (show_duplicate_btn) {
               element += `<a href="/core/slot/${data}/1" class="btn btn-light btn-sm mr-1" ` +
@@ -90,6 +118,7 @@ function init_datatable() {
                 `</button>`;
             }
           }
+          */
 
 
           return element;
