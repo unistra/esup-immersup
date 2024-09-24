eb44496f25d09373ae31319e70514fc3d5ed2a71
Christophe Boccheciampe <cb@unistra.fr>
2024-09-24 14:08:09 +0200

    clean some icons in case of group registrations

--- immersionlyceens/static/js/core/courses_slots.js
+++ immersionlyceens/static/js/core/courses_slots.js
@@ -19,8 +19,11 @@ function init_datatable() {
   var dt_columns = ""
 
   if (cohorts_only) {
+    let register_date_options = { dateStyle: 'long', timeStyle: 'short' };
 
     // Columns for cohorts slots pages
+    // Here a high school manager we can only view slots opened to cohorts and register his students
+    // Do not use for slots management
     dt_columns = [
       {
         data: 'id',
@@ -28,42 +31,17 @@ function init_datatable() {
           let element = ""
           let edit_mode = 0;
 
-          if (row.structure_code && row.structure_managed_by_me || row.highschool_label && row.highschool_managed_by_me || cohorts_only) {
-            if (show_duplicate_btn) {
-              element += `<a href="/core/slot/${data}/1" class="btn btn-light btn-sm mr-1" ` +
-                `title="${duplicate_text}"><i class="fa far fa-copy fa-2x centered-icon"></i></a>`;
-            }
-
-            // Future slot : edit button
-            if (row.is_past === false && show_modify_btn) {
-              element += `<a href="/core/slot/${data}" class="btn btn-light btn-sm mr-1" title="${modify_text}"><i class="fa fas fa-pencil fa-2x centered-icon"></i></a>\n`;
-            }
-
-            // Future slot and no registration : delete button
-            if (row.n_register === 0 && row.n_group_register === 0 && row.is_past === false && show_delete_btn) {
-              element += `<button class="btn btn-light btn-sm mr-1" onclick="deleteDialog.data('slot_id', ${data}).dialog('open')" title="${delete_text}"><i class="fa fas fa-trash fa-2x centered-icon"></i></button>\n`;
-            }
-
-            // if(row.attendances_value === attendance_to_enter) {
-            // Past slot with registrations : can update attendances
-            if (cohorts_only === false && row.is_past === true && (row.n_register > 0 || row.n_group_register > 0)) {
-              edit_mode = 1
-              element += `<button class="btn btn-light btn-sm mr-1" name="edit" onclick="open_modal(${data}, ${edit_mode}, ${row.n_places}, ${row.allow_individual_registrations}, ${row.allow_group_registrations}, ${row.group_mode}, ${row.n_group_places}, ${row.is_past}, ${row.can_update_registrations}, ${row.place})" title="${attendances_text}">` +
-                `<i class='fa fas fa-edit fa-2x centered-icon'></i>` +
-                `</button>`;
-            }
-            // no attendance to enter : can view registered users or groups
-            else if (row.attendances_value === attendance_not_yet || row.attendances_value === attendance_nothing_to_enter || row.n_register > 0 || row.n_group_register > 0) {
-              element += `<button class="btn btn-light btn-sm mr-1" name="view" onclick="open_modal(${data}, ${edit_mode}, ${row.n_places}, ${row.allow_individual_registrations}, ${row.allow_group_registrations}, ${row.group_mode}, ${row.n_group_places}, ${row.is_past}, ${row.can_update_registrations}, ${row.place})" title="${registered_text}">` +
-                `<i class='fa fas fa-eye fa-2x centered-icon'></i>` +
-                `</button>`;
-            }
-            if (cohorts_only === true) {
-              element += `<span data-toggle="tooltip" data-html="true" data-contrainer="body" title="${registration_date_limit_txt} : <br>${formatDate(row.registration_limit_date)}">` +
-                `<i class="fa fas fa-calendar pr-2"></i>` +
-                `</span>`;
-            }
+          // Future slot : register button
+          // TODO: add registration limit date test
+          if (row.is_past === false) {
+            element += `<button type="button" class="badge badge-pill badge-primary" name="view" onclick="open_modal(${data}, ${edit_mode}, ${row.n_places}, ${row.allow_individual_registrations}, ${row.allow_group_registrations}, ${row.group_mode}, ${row.n_group_places}, ${row.is_past}, ${row.can_update_registrations}, ${row.place})" title="${registered_text}">` +
+                       `${group_registrations_txt}` +
+                       `</button>`;
           }
+
+          element += `<br><span class="badge badge-pill badge-info">` +
+                     `  ${registration_date_limit_txt} : ${formatDate(row.registration_limit_date, register_date_options)}` +
+                     `</span>`
           return element;
         }
       },
@@ -209,7 +187,7 @@ function init_datatable() {
             element += `<input class="mass chk mr-1" type="checkbox" name="select_for_mass_update_${data}" value="${data}">`
           }
 
-          if (row.structure_code && row.structure_managed_by_me || row.highschool_label && row.highschool_managed_by_me || cohorts_only) {
+          if (row.structure_code && row.structure_managed_by_me || row.highschool_label && row.highschool_managed_by_me) {
             if (show_duplicate_btn) {
               element += `<a href="/core/slot/${data}/1" class="btn btn-light btn-sm mr-1" ` +
                 `title="${duplicate_text}"><i class="fa far fa-copy fa-2x centered-icon"></i></a>`;
@@ -222,27 +200,22 @@ function init_datatable() {
 
             // Future slot and no registration : delete button
             if (row.n_register === 0 && row.n_group_register === 0 && row.is_past === false && show_delete_btn) {
-              element += `<button class="btn btn-light btn-sm mr-1" onclick="deleteDialog.data('slot_id', ${data}).dialog('open')" title="${delete_text}"><i class="fa fas fa-trash fa-2x centered-icon"></i></button>\n`;
+              element += `<button type="button" class="btn btn-light btn-sm mr-1" onclick="deleteDialog.data('slot_id', ${data}).dialog('open')" title="${delete_text}"><i class="fa fas fa-trash fa-2x centered-icon"></i></button>\n`;
             }
 
             // if(row.attendances_value === attendance_to_enter) {
             // Past slot with registrations : can update attendances
-            if (cohorts_only === false && row.is_past === true && (row.n_register > 0 || row.n_group_register > 0)) {
+            if (row.is_past === true && (row.n_register > 0 || row.n_group_register > 0)) {
               edit_mode = 1
-              element += `<button class="btn btn-light btn-sm mr-1" name="edit" onclick="open_modal(${data}, ${edit_mode}, ${row.n_places}, ${row.allow_individual_registrations}, ${row.allow_group_registrations}, ${row.group_mode}, ${row.n_group_places}, ${row.is_past}, ${row.can_update_registrations}, ${row.place})" title="${attendances_text}">` +
-                `<i class='fa fas fa-edit fa-2x centered-icon'></i>` +
-                `</button>`;
+              element += `<button type="button" class="btn btn-light btn-sm mr-1" name="edit" onclick="open_modal(${data}, ${edit_mode}, ${row.n_places}, ${row.allow_individual_registrations}, ${row.allow_group_registrations}, ${row.group_mode}, ${row.n_group_places}, ${row.is_past}, ${row.can_update_registrations}, ${row.place})" title="${attendances_text}">` +
+                         `  <i class='fa fas fa-edit fa-2x centered-icon'></i>` +
+                         `</button>`;
             }
             // no attendance to enter : can view registered users or groups
             else if (row.attendances_value === attendance_not_yet || row.attendances_value === attendance_nothing_to_enter || row.n_register > 0 || row.n_group_register > 0) {
-              element += `<button class="btn btn-light btn-sm mr-1" name="view" onclick="open_modal(${data}, ${edit_mode}, ${row.n_places}, ${row.allow_individual_registrations}, ${row.allow_group_registrations}, ${row.group_mode}, ${row.n_group_places}, ${row.is_past}, ${row.can_update_registrations}, ${row.place})" title="${registered_text}">` +
-                `<i class='fa fas fa-eye fa-2x centered-icon'></i>` +
-                `</button>`;
-            }
-            if (cohorts_only === true) {
-              element += `<span data-toggle="tooltip" data-html="true" data-contrainer="body" title="${registration_date_limit_txt} : <br>${formatDate(row.registration_limit_date)}">` +
-                `<i class="fa fas fa-calendar pr-2"></i>` +
-                `</span>`;
+              element += `<button type="button" class="btn btn-light btn-sm mr-1" name="view" onclick="open_modal(${data}, ${edit_mode}, ${row.n_places}, ${row.allow_individual_registrations}, ${row.allow_group_registrations}, ${row.group_mode}, ${row.n_group_places}, ${row.is_past}, ${row.can_update_registrations}, ${row.place})" title="${registered_text}">` +
+                         `  <i class='fa fas fa-eye fa-2x centered-icon'></i>` +
+                         `</button>`;
             }
           }
           return element;
