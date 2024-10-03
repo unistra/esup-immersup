2c021180110eeb1ceeef5826a6eae5784f91ca7a
Christophe Boccheciampe <cb@unistra.fr>
2024-10-03 14:34:52 +0200

    Fix cohort slot registration limit date for ref-lyc

--- immersionlyceens/static/js/core/courses_slots.js
+++ immersionlyceens/static/js/core/courses_slots.js
@@ -30,7 +30,6 @@ function init_datatable() {
         render: function (data, type, row) {
           let element = ""
           let edit_mode = 0;
-          let can_register = row.registration_limit_date_is_past === false
           let badge_class = "badge-info"
           let valid_restrictions = false
 
@@ -44,23 +43,23 @@ function init_datatable() {
             valid_restrictions = true
           }
 
+          let can_register = valid_restrictions && !row.is_past && !row.registration_limit_date_is_past && row.valid_registration_start_date
+
           // Future slot : register button
-          if (row.is_past === false && valid_restrictions === true) {
-            if(row.valid_registration_start_date) {
-              element += `<button type="button" class="badge badge-pill badge-primary" name="view" onclick="open_modal(${data}, ${edit_mode}, ${row.n_places}, ${row.allow_individual_registrations}, ${row.allow_group_registrations}, ${row.group_mode}, ${row.n_group_places}, ${row.is_past}, ${row.can_update_registrations}, ${row.place})" title="${registered_text}">` +
-                         `  ${register_groups_txt}` +
-                         `</button>`;
-            }
-            else {
-              element += `<span class="badge badge-pill badge-info">` +
-                        `  ${registration_start_date_txt} : ${formatDate(row.registration_start_date, register_date_options)}` +
-                        `</span>`
-            }
+          if (can_register) {
+            element += `<button type="button" class="badge badge-pill badge-primary" name="view" onclick="open_modal(${data}, ${edit_mode}, ${row.n_places}, ${row.allow_individual_registrations}, ${row.allow_group_registrations}, ${row.group_mode}, ${row.n_group_places}, ${row.is_past}, ${row.can_update_registrations}, ${row.place})" title="${registered_text}">` +
+                       `  ${register_groups_txt}` +
+                       `</button>`;
+          }
+          else if(!row.valid_registration_start_date) {
+            element += `<span class="badge badge-pill badge-info">` +
+                       `  ${registration_start_date_txt} : ${formatDate(row.registration_start_date, register_date_options)}` +
+                       `</span>`
           }
 
           element += element === "" ? "" : "<br>"
 
-          if(can_register === false) {
+          if(row.registration_limit_date_is_past) {
             badge_class = "badge-danger"
           }
 
