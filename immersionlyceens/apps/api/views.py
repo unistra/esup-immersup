bc66034d50ec427740355df2ee3ff955c51395c5
v.beaucourt <v.beaucourt@unistra.fr>
2025-10-01 09:00:18 +0200

    Ce ne sont pas les commits que vous recherchez

--- immersionlyceens/apps/api/views.py
+++ immersionlyceens/apps/api/views.py
@@ -373,17 +373,18 @@ def validate_parent_date(request):
     :return: a dict with data about the date
     """
     response = {'data': {}, 'msg': ''}
+    details = []
 
     _date = request.GET.get('date')
 
-    start_date_to_convert = request.GET.get('start_date')
-    end_date_to_convert = request.GET.get('end_date')
-
-    start_date = datetime.datetime.fromisoformat(start_date_to_convert) if start_date_to_convert else None
-    end_date = datetime.datetime.fromisoformat(end_date_to_convert) if end_date_to_convert else None
-
-    is_before = False
-    is_after = False
+    try:
+        start_date = datetime.datetime.fromisoformat(request.GET.get('start_date'))
+    except ValueError:
+        start_date = None
+    try:
+        end_date = datetime.datetime.fromisoformat(request.GET.get('end_date'))
+    except ValueError:
+        end_date = None
 
     if not _date:
         response['msg'] = gettext('Error: A date is required')
@@ -399,13 +400,10 @@ def validate_parent_date(request):
             response['msg'] = gettext('Error: bad date format')
             return JsonResponse(response, safe=False)
 
-    formated_date =  timezone.make_aware(formated_date)
+    formated_date = timezone.make_aware(formated_date)
 
-    details = []
-    if start_date:
-        is_before = formated_date < start_date
-    if end_date:
-        is_after = formated_date > end_date
+    is_before = formated_date < start_date if start_date else False
+    is_after = formated_date > end_date if end_date else False
 
     if is_before:
         details.append(_("Before"))
