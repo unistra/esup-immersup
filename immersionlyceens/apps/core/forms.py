0b4d47bf654921b2715213aa6dc586e369969b1d
Christophe Boccheciampe <cb@unistra.fr>
2025-10-02 16:45:24 +0200

    Course : update min/max date constraints (ignore all past slots)

--- immersionlyceens/apps/core/forms.py
+++ immersionlyceens/apps/core/forms.py
@@ -87,6 +87,8 @@ class CourseForm(forms.ModelForm):
 
 
     def clean(self):
+        now = timezone.now()
+
         cleaned_data = super().clean()
         publication_start = cleaned_data.get('start_date')
         publication_end = cleaned_data.get('end_date')
@@ -113,26 +115,31 @@ class CourseForm(forms.ModelForm):
         # get min/max course slot dates and forbid publication dates outside this interval
         if self.instance:
             if publication_start:
-                slot_min = self.instance.slots.order_by('date', 'start_time').first()
-                slot_min_start_datetime = timezone.make_aware(datetime.combine(slot_min.date, slot_min.start_time))
-                slot_min_start_time = timezone.make_aware(slot_min.start_time)
-                if publication_start > slot_min_start_datetime:
-                    raise forms.ValidationError(
-                        _("""There is a slot that starts on %s at %s, """
-                          """the publication start date must be before this slot.""")
-                        % (slot_min.date, slot_min_start_time)
-                    )
+                # First slot in the future (we can safely ignore past slots)
+                slot_min = self.instance.slots.filter(date__gte=now.date()).order_by('date', 'start_time').first()
+
+                if slot_min:
+                    slot_min_start_datetime = timezone.make_aware(datetime.combine(slot_min.date, slot_min.start_time))
+                    slot_min_start_time = timezone.make_aware(slot_min.start_time)
+                    if publication_start > slot_min_start_datetime:
+                        raise forms.ValidationError(
+                            _("""There is a slot that starts on %s at %s, """
+                              """the publication start date must be before this slot.""")
+                            % (slot_min.date, slot_min_start_time)
+                        )
 
             if publication_end:
-                slot_max = self.instance.slots.order_by('date', 'end_time').last()
-                slot_max_end_datetime = timezone.make_aware(datetime.combine(slot_max.date, slot_max.start_time))
-                slot_max_end_time = timezone.make_aware(slot_max.end_time)
-                if publication_end < slot_max_end_datetime:
-                    raise forms.ValidationError(
-                        _("""There is a slot that ends on %s at %s, """
-                          """the publication end date must be after the end of this slot.""")
-                        % (slot_max.date, slot_max_end_time)
-                    )
+                slot_max = self.instance.slots.filter(date__gte=now.date()).order_by('date', 'end_time').last()
+
+                if slot_max:
+                    slot_max_end_datetime = timezone.make_aware(datetime.combine(slot_max.date, slot_max.start_time))
+                    slot_max_end_time = timezone.make_aware(slot_max.end_time)
+                    if publication_end < slot_max_end_datetime:
+                        raise forms.ValidationError(
+                            _("""There is a slot that ends on %s at %s, """
+                              """the publication end date must be after the end of this slot.""")
+                            % (slot_max.date, slot_max_end_time)
+                        )
 
         # Check user rights
         if self.request:
